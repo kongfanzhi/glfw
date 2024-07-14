91a6a4db92d29d9205f40b0c0cd87f9d8b9a47a5
kongfanzhi <463120197@qq.com>
2024-07-15 01:11:24 +0800

    Set OpenGL

--- premake5.lua
+++ premake5.lua
@@ -31,7 +31,6 @@ project "GLFW"
     }
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
         
@@ -56,5 +55,11 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
 		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
